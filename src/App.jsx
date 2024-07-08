37ad53d816bd032c5b8032ba1aa82231096435ef
Shaleen Chowdhary <137061973+shaleenchowdhary@users.noreply.github.com>
2024-07-08 23:53:21 +0530

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Search from './components/search/Search';
+
+function App() {
+  return (
+    <div className="">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
